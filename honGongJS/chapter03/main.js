835dde5d56bf83b88cbf4ff4eeca0c47b2f81f6d
sykang013 <sy.kang013@gmail.com>
2023-02-19 22:17:39 +0900

    solve. 챕터3 문제5

--- honGongJS/chapter03/main.js
+++ honGongJS/chapter03/main.js
@@ -1,12 +1,22 @@
 //문제 4
-const a = Number(prompt("숫자를 입력해주세요",""))
+// const a = Number(prompt("숫자를 입력해주세요",""))
 
-if (a > 0) {
-    alert("입력한 숫자는 양수입니다.")
-} else if (a < 0) {
-    alert("입력한 숫자는 음수입니다.")
-} else if ( a == 0) {
-    alert("입력한 숫자는 0 입니다.")
+// if (a > 0) {
+//     alert("입력한 숫자는 양수입니다.")
+// } else if (a < 0) {
+//     alert("입력한 숫자는 음수입니다.")
+// } else if ( a == 0) {
+//     alert("입력한 숫자는 0 입니다.")
+// }
+
+//문제 5
+const a = Number(prompt("0이 아닌 숫자를 입력해주세요.", ""))
+let rest = a % 2
+
+if (rest == 0){
+    alert("짝수입니다.")
+} else if (rest == 1) {
+    alert("홀수입니다.")
+} else {
+    alert("ERROR: 양수를 입력해주세요.")
 }
-
-
