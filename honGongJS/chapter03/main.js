2377d5a805999fa532b5ef3202d4e7bc26f27bd9
sykang013 <sy.kang013@gmail.com>
2023-02-19 23:08:42 +0900

    solve. 챕터3-2 문제2

--- honGongJS/chapter03/main.js
+++ honGongJS/chapter03/main.js
@@ -22,15 +22,40 @@
 // }
 
 //문제 6
-const month = Number(prompt("월을 입력해주세요",""))
+// const month = Number(prompt("월을 입력해주세요",""))
 
-if (month >= 3 && month <= 5){
-    alert("봄입니다.")
-} else if(month >= 6 && month <= 8){
-    alert("여름입니다.")
-} else if(month >= 9 && month <= 11){
-    alert("가을입니다.")
-} else if(month == 12 || month <= 2){
-    alert("겨울입니다.")
+// if (month >= 3 && month <= 5){
+//     alert("봄입니다.")
+// } else if(month >= 6 && month <= 8){
+//     alert("여름입니다.")
+// } else if(month >= 9 && month <= 11){
+//     alert("가을입니다.")
+// } else if(month == 12 || month <= 2){
+//     alert("겨울입니다.")
+// }
+
+/* --------------------------------- /챕터 3-2 -------------------------------- */
+
+//문제 2
+
+const rawInput = prompt("태어난 해를 입력해주세요", "")
+const year = Number(rawInput)
+const rest = year % 12
+
+let result 
+switch(rest){
+    case 0: result = "원숭이"; break;
+    case 1: result = "닭"; break;
+    case 2: result = "개"; break;
+    case 3: result = "돼지"; break;
+    case 4: result = "쥐"; break;
+    case 5: result = "소"; break;
+    case 6: result = "호랑이"; break;
+    case 7: result = "토끼"; break;
+    case 8: result = "용"; break;
+    case 9: result = "뱀"; break;
+    case 10: result = "말"; break;
+    case 11: result = "양"; break;
 }
 
+alert(`${year}년에 태어났다면 ${result} 띠입니다.`)
