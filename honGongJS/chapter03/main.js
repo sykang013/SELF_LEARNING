95617e179d2daf68f8f5d6d823b93cc4c9d7d5bf
sykang013 <sy.kang013@gmail.com>
2023-02-19 22:13:23 +0900

    solve. 챕터3 문제4

---
+++ honGongJS/chapter03/main.js
@@ -0,0 +1,12 @@
+//문제 4
+const a = Number(prompt("숫자를 입력해주세요",""))
+
+if (a > 0) {
+    alert("입력한 숫자는 양수입니다.")
+} else if (a < 0) {
+    alert("입력한 숫자는 음수입니다.")
+} else if ( a == 0) {
+    alert("입력한 숫자는 0 입니다.")
+}
+
+
