021fee6b884359a0d7525685e670d7f3896738b7
sykang013 <sy.kang013@gmail.com>
2023-02-19 23:42:18 +0900

    config. main브랜치 merge를 위한 커밋

--- honGongJS/chapter03/main.js
+++ honGongJS/chapter03/main.js
@@ -38,24 +38,24 @@
 
 //문제 2
 
-const rawInput = prompt("태어난 해를 입력해주세요", "")
-const year = Number(rawInput)
-const rest = year % 12
+// const rawInput = prompt("태어난 해를 입력해주세요", "")
+// const year = Number(rawInput)
+// const rest = year % 12
 
-let result 
-switch(rest){
-    case 0: result = "원숭이"; break;
-    case 1: result = "닭"; break;
-    case 2: result = "개"; break;
-    case 3: result = "돼지"; break;
-    case 4: result = "쥐"; break;
-    case 5: result = "소"; break;
-    case 6: result = "호랑이"; break;
-    case 7: result = "토끼"; break;
-    case 8: result = "용"; break;
-    case 9: result = "뱀"; break;
-    case 10: result = "말"; break;
-    case 11: result = "양"; break;
-}
+// let result 
+// switch(rest){
+//     case 0: result = "원숭이"; break;
+//     case 1: result = "닭"; break;
+//     case 2: result = "개"; break;
+//     case 3: result = "돼지"; break;
+//     case 4: result = "쥐"; break;
+//     case 5: result = "소"; break;
+//     case 6: result = "호랑이"; break;
+//     case 7: result = "토끼"; break;
+//     case 8: result = "용"; break;
+//     case 9: result = "뱀"; break;
+//     case 10: result = "말"; break;
+//     case 11: result = "양"; break;
+// }
 
-alert(`${year}년에 태어났다면 ${result} 띠입니다.`)
+// alert(`${year}년에 태어났다면 ${result} 띠입니다.`)
