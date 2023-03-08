011753123582c0f0c76ee260ee5ae360e395b432
sykang013 <sy.kang013@gmail.com>
2023-03-08 13:22:43 +0900

    learn. initialOrderList 작성

--- likeLion-react/11-recoil-suyeong/learning-recoil/src/@store/orderListState.js
+++ likeLion-react/11-recoil-suyeong/learning-recoil/src/@store/orderListState.js
@@ -1,9 +1,16 @@
-import { atom } from 'recoil';
+import { atom, atomFamily } from 'recoil';
 
-export const orderListState = atom({
-  key: 'orderListState',
-  default: [
+const initialOrderList = [
     { id: 'order-1', order: '아메리카노', price: 4800 },
     { id: 'order-2', order: '그린티 프라푸치노', price: 6200 },
-  ],
+]
+
+export const orderListState = atom ({
+  key: 'orderListState',
+  default: initialOrderList,
+});
+
+export const orderListFamily = atomFamily ({
+  key: 'orderListState',
+  default: (id) => initialOrderList.finn((order) =>  order.id === id),
 });
--- likeLion-react/11-recoil-suyeong/learning-recoil/src/OrderListPage.jsx
+++ likeLion-react/11-recoil-suyeong/learning-recoil/src/OrderListPage.jsx
@@ -1,7 +1,24 @@
+import { useRecoilValue } from 'recoil';
+import { orderListFamily } from './@store/orderListState';
+
 export default function OrderListPage() {
+  const beverage = useRecoilValue(orderListFamily('order-2'));
+
   return (
     <div>
-      <h2>주문 목록</h2>
+      <h2>주문한 음료</h2>
+      <div>
+        <h3>{beverage.order}</h3>
+        <span>{currencyKR(beverage.price)}</span>
+      </div>
     </div>
   );
 }
+
+const numberWithComma = (n) => {
+  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
+
+const currencyKR = (value) => {
+  return `${numberWithComma(value)}원`;
+};
